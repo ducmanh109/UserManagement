7089a59644a3dd33c9155ccee29576d6ac276cbb
haducmanh10 <haducmanh10@gmail.com>
2023-03-24 17:44:00 +0700

    build: init project

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import React, {useEffect} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {SETUP_API} from './src/api/api.config';
+import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { SETUP_API } from './src/api/api.config';
 import RootStack from './src/stacks/RootStack';
-import {observer} from 'mobx-react';
+import { observer } from 'mobx-react';
 
 const App = () => {
   useEffect(() => {
--- src/api/api.config.ts
+++ src/api/api.config.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {SERVERS} from './api.servers';
+import { SERVERS } from './api.servers';
 
 const axiosDefault = axios.defaults;
 
